cd4495bd83bf95916646aa3084b669281d448808
FrancoBracco34 <terabitfra@gmail.com>
2026-05-20 19:41:36 +0200

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.capogrillocottage.it',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
